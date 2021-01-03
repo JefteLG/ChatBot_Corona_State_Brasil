3113d80c877aa8b7eeca9af485023571c6ba2207
JefteLG <jeftelg72@gmail.com>
2021-01-03 15:41:38 -0300

    ajustes na actions_status_corona_state

--- actions.py
+++ actions.py
@@ -55,18 +55,19 @@ class ActionsStatusCoronaState(Action):
                 state = e['value']
                 print(state)
 
-        message = 'Não consegui localizar este estado...\n' \
-                  'Verifique o nome do estado que você escreveu.\n' \
-                  'Eu ainda estou aprendendo tente colocar o nome do estado com a primeira letra maiuscula\n' \
-                  'Ex.:"Rio de Janeiro", "Piaui", "Acre"'
+        message =\
+            f'Você digitou: {state}, está correto?\n\n' \
+            'Hum... que coisa não? eu acho que você esqueceu ou errou alguma letra. Não consegui localizar este ' \
+            f'estado({state})\n' \
 
         for data in response['data']:
             if remove_non_ascii_normalized(data['state']) == remove_non_ascii_normalized(state):
                 print(data)
-                message = f"Estado: {data['state']}\n" \
-                          f"Casos confirmados: {data['cases']}\n" \
-                          f"Supeitos: {data['suspects']}\nMortes: {data['deaths']}\n" \
-                          f"Ultima atualização: {data['datetime']}"
+                message =\
+                    f"Estado: {data['state']}\n" \
+                    f"Casos confirmados: {data['cases']}\n" \
+                    f"Supeitos: {data['suspects']}\nMortes: {data['deaths']}\n" \
+                    f"Ultima atualização: {data['datetime']}"
 
         dispatcher.utter_message(text=message)
 
